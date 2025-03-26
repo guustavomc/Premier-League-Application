f1e7dda16e072ec94f0fc51818e462b79eccca4b
guustavomc <gustavomccb@gmail.com>
2025-03-25 22:29:00 -0300

    refactored selectedAction

--- src/main/java/org/example/actionSelector.java
+++ src/main/java/org/example/actionSelector.java
@@ -9,25 +9,16 @@ public class actionSelector {
     private String filePath = "src/main/java/data/data.json";
     private boolean selectorStillInUse=true;
     public void actionSelector() {
-
-
         while (selectorStillInUse) {
             Scanner scanner = new Scanner(System.in);
 
-            /*System.out.println("1 - Print Clubs by Position");
-            System.out.println("2 - Print Clubs by Wins");
-            System.out.println("3 - Print Clubs by Losses");
-            System.out.println("4 - Print Clubs by Goals Scored");
-            System.out.println("5 - Print Fixtures");
-            System.out.println("6 - End operations");*/
-
-            System.out.println("Print Clubs by Position");
-            System.out.println("Print Clubs by Wins");
-            System.out.println("Print Clubs by Losses");
-            System.out.println("Print Clubs by Goals Scored");
-            System.out.println("Print Fixtures");
-            System.out.println("End operations");
-
+            System.out.println(
+                    "Print Clubs by Position\n"+
+                    "Print Clubs by Wins\n"+
+                    "Print Clubs by Losses\n"+
+                    "Print Clubs by Goals Scored\n"+
+                    "Print Fixtures\n"+
+                    "End operations");
             System.out.println("Your Choice:");
             String valueSelected = scanner.nextLine();
             String valueSelectedCorrectly = valueSelected.toUpperCase().replaceAll("\\s+", "").trim();
@@ -40,50 +31,75 @@ public class actionSelector {
     private void selectedAction(String valueSelectedCorrectly) {
         switch(valueSelectedCorrectly){
             case "PRINTCLUBSBYPOSITION":
-                List<Club> clubsArrayPosition =  new readClubs(filePath).getClubList();
-                clubsArrayPosition.sort(Comparator.comparingInt(Club::getPosition));
-                System.out.println("Print Clubs by Position");
-                for(Club club: clubsArrayPosition){
-                    club.printName();
-                }
+                printClubsByPosition();
                 break;
             case "PRINTCLUBSBYWINS":
-                List<Club> clubsArrayWins =  new readClubs(filePath).getClubList();
-                clubsArrayWins.sort(Comparator.comparingInt(Club::getWins).reversed());
-                System.out.println("\nPrint Clubs by Wins");
-                for(Club club: clubsArrayWins){
-                    club.printName();
-                }
+                printClubsByWins();
                 break;
             case "PRINTCLUBSBYLOSSES":
-                List<Club> clubsArrayLosses =  new readClubs(filePath).getClubList();
-                clubsArrayLosses.sort(Comparator.comparingInt(Club::getLosses).reversed());
-                System.out.println("\nPrint Clubs by Losses");
-                for(Club club: clubsArrayLosses){
-                    club.printName();
-                }
+                printClubsByLosses();
                 break;
             case "PRINTCLUBSBYGOALSSCORED":
-                List<Club> clubsArrayGoalsScored =  new readClubs(filePath).getClubList();
-                clubsArrayGoalsScored.sort(Comparator.comparingInt(Club::getGoalsScored).reversed());
-                System.out.println("\nPrint Clubs by Goals Scored");
-                for(Club club: clubsArrayGoalsScored){
-                    club.printName();
-                }
+                printClubsByGoalsScored();
                 break;
             case "PRINTFIXTURES":
-                List<Club> clubsArray =  new readClubs(filePath).getClubList();
-                List<Fixture> fixtureArray = new readFixtures(filePath, clubsArray).getFixturesList();
-                System.out.println("\nPrint Fixtures");
-                for (Fixture fixture: fixtureArray){
-                    fixture.printFixture();
-                }
+                printFixtures();
                 break;
             case "ENDOPERATIONS":
                 System.out.println("\nEnding operations");
                 selectorStillInUse=false;
 
                 break;
+        }
+    }
+
+    private void printFixtures() {
+        List<Club> clubsArray = getClubsArray();
+        List<Fixture> fixtureArray = new readFixtures(filePath, clubsArray).getFixturesList();
+        System.out.println("\nPrint Fixtures");
+        for (Fixture fixture: fixtureArray){
+            fixture.printFixture();
+        }
+    }
+
+    private List<Club> getClubsArray() {
+        List<Club> clubsArray =  new readClubs(filePath).getClubList();
+        return clubsArray;
+    }
+
+    private void printClubsByGoalsScored() {
+        List<Club> clubsArrayGoalsScored =  getClubsArray();
+        clubsArrayGoalsScored.sort(Comparator.comparingInt(Club::getGoalsScored).reversed());
+        System.out.println("\nPrint Clubs by Goals Scored");
+        for(Club club: clubsArrayGoalsScored){
+            club.printName();
+        }
+    }
+
+    private void printClubsByLosses() {
+        List<Club> clubsArrayLosses =  getClubsArray();
+        clubsArrayLosses.sort(Comparator.comparingInt(Club::getLosses).reversed());
+        System.out.println("\nPrint Clubs by Losses");
+        for(Club club: clubsArrayLosses){
+            club.printName();
+        }
+    }
+
+    private void printClubsByWins() {
+        List<Club> clubsArrayWins =  getClubsArray();
+        clubsArrayWins.sort(Comparator.comparingInt(Club::getWins).reversed());
+        System.out.println("\nPrint Clubs by Wins");
+        for(Club club: clubsArrayWins){
+            club.printName();
+        }
+    }
+
+    private void printClubsByPosition() {
+        List<Club> clubsArrayPosition =  getClubsArray();
+        clubsArrayPosition.sort(Comparator.comparingInt(Club::getPosition));
+        System.out.println("Print Clubs by Position");
+        for(Club club: clubsArrayPosition){
+            club.printName();
         }
     }
 
